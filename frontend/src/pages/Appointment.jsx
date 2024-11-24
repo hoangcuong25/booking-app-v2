4f2ee9c5f3419ddf04a0aef9cce54ccdbc2120fb
hoang cuong <vancuong25112003@gmail.com>
2024-11-24 18:31:53 +0700

    update

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-key */
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { assets, doctors } from '../assets/assets'
@@ -5,17 +6,90 @@ import { assets, doctors } from '../assets/assets'
 const Appointment = () => {
 
     const { docId } = useParams()
+
     const [docInfo, setDocInfo] = useState()
+
+    const [slotBook, setSlotBook] = useState({})
 
     const getDocInfo = () => {
         setDocInfo(doctors.find((i) => docId === i._id))
     }
 
+    const bookingSlots = async () => {
+        let today = new Date()
+        let bookingDate = []
+        let bookingTime = []
+
+        let currentDate = new Date()
+        for (let i = 0; i < 7; i++) {
+            currentDate = today.getDate() + i
+            bookingDate.push(currentDate)
+
+            if (currentDate === today.getDate()) {
+                if (today.getHours() > 9 && today.getHours() < 21) {
+                    if (today.getMinutes() > 30) {
+                        bookingTime.push([{
+                            hour: today.getHours() + 1,
+                            minute: 0,
+                        }])
+                    }
+                    else {
+                        bookingTime.push([{
+                            hour: today.getHours(),
+                            minute: 30,
+                        }])
+                    }
+                }
+            }
+            else {
+                bookingTime.push([{
+                    hour: 9,
+                    minute: 0,
+                }])
+            }
+
+            if (currentDate === today.getDate()) {
+                if (bookingTime[0][0].minute === 30) {
+                    bookingTime[0]?.push({
+                        hour: today.getHours() + 1,
+                        minute: 0,
+                    })
+                } else {
+                    bookingTime[0]?.push({
+                        hour: bookingTime[0][0].hour,
+                        minute: 30,
+                    })
+                }
+                for (let i = 1; i < 15; i++) {
+                    if (bookingTime[0][i]?.hour < 21) {
+                        if (bookingTime[0][i]?.minute === 30) {
+                            bookingTime[0]?.push({
+                                hour: bookingTime[0][i]?.hour + 1,
+                                minute: 0,
+                            })
+                        } else {
+                            bookingTime[0]?.push({
+                                hour: bookingTime[0][i]?.hour,
+                                minute: 30,
+                            })
+                        }
+                    }
+                }
+            }
+        }
+
+        await setSlotBook({
+            date: bookingDate,
+            time: bookingTime
+        })
+    }
+
     useEffect(() => {
         getDocInfo()
+        bookingSlots()
     }, [docId])
 
-    console.log(docInfo)
+    console.log('SlotBook', slotBook)
 
     return (
         <div className='mt-5'>
@@ -24,24 +98,48 @@ const Appointment = () => {
                     <img src={docInfo?.image} alt="" />
                 </div>
 
-                <div className='border w-full rounded-md border-gray-400 py-3.5 px-6'>
-                    <div className='flex items-center gap-3.5'>
-                        <p className='text-xl text-gray-800'>{docInfo?.name}</p>
-                        <img src={assets.verified_icon} className='w-5' alt="" />
+                <div className='flex flex-col w-full relative'>
+                    <div className='border w-full h-full rounded-md border-gray-400 py-3.5 px-6'>
+                        <div className='flex items-center gap-3.5'>
+                            <p className='text-xl text-gray-800'>{docInfo?.name}</p>
+                            <img src={assets.verified_icon} className='w-5' alt="" />
+                        </div>
+                        <div className='flex gap-5'>
+                            <p>{docInfo?.degree} - {docInfo?.speciality}</p>
+                            <button className='text-sm text-gray-600 w-20 border border-primary rounded-full'>{docInfo?.experience}</button>
+                        </div>
+                        <div className='flex gap-2'>
+                            <p className='text-gray-800 font-medium'>about</p>
+                            <img src={assets.info_icon} alt="" />
+                        </div>
+                        <p className='text-sm text-gray-600'>{docInfo?.about}</p>
+                        <p className='mt-3 text-gray-600'>Appointment fee: <span className='text-gray-800'>${docInfo?.fees}</span></p>
                     </div>
-                    <div className='flex gap-5'>
-                        <p>{docInfo?.degree} - {docInfo?.speciality}</p>
-                        <button className='text-sm text-gray-600 w-20 border border-primary rounded-full'>{docInfo?.experience}</button>
-                    </div>
-                    <div className='flex gap-2'>
-                        <p className='text-gray-800 font-medium'>about</p>
-                        <img src={assets.info_icon} alt="" />
-                    </div>
-                    <p className='text-sm text-gray-600'>{docInfo?.about}</p>
-                    <p className='mt-3 text-gray-600'>Appointment fee: <span className='text-gray-800'>${docInfo?.fees}</span></p>
                 </div>
             </div>
-        </div>
+
+            <div className='mt-6'>
+                <p>Booking Slots</p>
+                <div className='flex gap-8 mt-2'>
+                    {slotBook?.date?.map((item, index) => (
+                        <div key={index} className='flex items-center justify-center border border-primary rounded-full w-14 h-20 cursor-pointer'>
+                            <p>{item}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className='flex gap-8 mt-5'>
+                    {slotBook?.time?.[0].map((item, index) => (
+                        <div key={index} className='flex items-center justify-center border border-gray-400 rounded-full w-20 h-10 cursor-pointer'>
+                            {
+                                item.hour > 12 ?
+                                    <p className='text-sm'>{item.hour}:{item.minute} PM</p>
+                                    : <p className='text-sm'>{item.hour}:{item.minute} AM</p>
+                            }
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div >
     )
 }
 

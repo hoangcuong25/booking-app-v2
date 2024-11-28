87d96d808e0965ab36d5aa40d0371031bfc51e1f
hoang cuong <vancuong25112003@gmail.com>
2024-11-28 16:24:32 +0700

    update

---
+++ backend/controllers/userController.js
@@ -0,0 +1,6 @@
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+import userModel from '../models/userModel.js'
+import jwt from 'jsonwebtoken'
+import { v2 as cloudinary } from 'cloudinary'
+
---
+++ backend/routes/userRoute.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import upload from '../middlewares/multer.js'
+
+const userRouter = express.Router()
+
+export default userRouter
